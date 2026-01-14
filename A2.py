07073cb95e237abaf5fbcdd36d63eece96a39ba9
Abhishek Verma <Vermad3205@gmail.com>
2026-01-14 14:59:40 +0530

    change in A2

--- A2.py
+++ A2.py
@@ -1,9 +1,50 @@
+
+
+name = input("Enter your name: ")
+print(f"Hello", name)
+age = input("Enter your age: ")
+print("age is: ",age)
+
+
 a=15
 b=4
+# question 1
 print("Addition:",a+b)
 print("Subtraction:",a-b)
 print("multiplication:",a*b)
 print("Division:",a/b)
+# question 2
 print("Floor Division:",a//b)
-print("reminer:",a%b)
+print("reminder:",a%b)
 print("power :",a ** b)
+
+# question 3
+
+
+Pen_price=15
+notebook_price=40
+
+Pens=3
+notebook=2
+
+total_cost=(Pen_price*Pens)+(notebook_price*notebook)
+
+print("Total cost is:",total_cost)
+
+
+#Question 4
+
+total = 0
+
+for i in range(5):
+    num = int(input("Enter a number: "))
+    total += num
+
+avg = total / 5
+print("Average =", avg)
+
+
+# Question 5
+
+
+
