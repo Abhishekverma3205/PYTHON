2d8602f443cc138ac8453873b7dd33e7ddc20720
Abhishek Verma <Vermad3205@gmail.com>
2026-01-14 14:44:39 +0530

    first commit

---
+++ A2.py
@@ -0,0 +1,9 @@
+a=15
+b=4
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("multiplication:",a*b)
+print("Division:",a/b)
+print("Floor Division:",a//b)
+print("reminer:",a%b)
+print("power :",a ** b)
---
+++ A3.py
@@ -0,0 +1,8 @@
+total = 0
+
+for i in range(5):
+    num = int(input("Enter a number: "))
+    total += num
+
+avg = total / 5
+print("Average =", avg)
