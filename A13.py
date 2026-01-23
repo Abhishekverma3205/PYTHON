60da184f4aab49c6384c6b51b89d2369485a87b9
Abhishek Verma <Vermad3205@gmail.com>
2026-01-23 16:45:54 +0530

    Module_Math

---
+++ A13.py
@@ -0,0 +1,46 @@
+#Module
+
+import datetime
+import math
+now = datetime.datetime.now()
+print("Now:",now)
+
+#find the square root of 625
+print(math.sqrt(625))
+
+#caculate the factorial of 6
+print(math.factorial(6))
+
+#find the floor and ceiling values of 4.7
+
+print(math.floor(4.7))
+print(math.ceil(4.7))
+
+#compute 5 raised to the power 3 the math module
+
+print(math.pow(5,3))
+
+#find the absolute value of -18 using math.fabs()
+print(math.fabs(-18))
+
+#find the sin, cos, tan of 90 degrees(convert degrees to radians)
+
+rad = math.radians(90)
+print(math.sin(rad))
+print(math.cos(rad))
+print(math.tan(rad))
+
+#find the greatest common divisor(GCD) of 36 and 60
+
+print(math.gcd(36, 60))
+
+#find the value of e^3 using the math 
+print(math.exp(3)) 
+
+#find the logarithm base 10 of 1000
+
+print(math.log10(1000))
+
+#convert 180 degrees into radians 
+
+print(math.radians(180))
